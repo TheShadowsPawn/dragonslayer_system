ced8f69ba308bdf788c3c26fc6e8a33a2676d966
WeppsJr <153624353+WeppsJr@users.noreply.github.com>
2024-09-16 16:00:34 -0500

    Update combat.js

--- src/module/combat.js
+++ src/module/combat.js
@@ -29,7 +29,8 @@ const DslCombat = {
     // Roll init
     for (const group in groups) {
       // Object.keys(groups).forEach((group) => {
-      const roll = new Roll("1d6").evaluate;
+      const roll = new Roll("1d6");
+      await roll.evaluate();
       await roll.toMessage({
         flavor: game.i18n.format("DSL.roll.initiative", {
           group: CONFIG.DSL.colors[group],
